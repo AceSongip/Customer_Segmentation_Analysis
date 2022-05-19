cd2fac521843b0409f13d258ed62c5839c123ed9
Mohamad Ehsan <a165885@siswa.ukm.edu.my>
2022-05-19 18:53:38 +0800

    Updated Script
    Inside customer training, I put code to see the relationship between each features with the target variable by using seaborn plotting.

--- customer_training.py
+++ customer_training.py
@@ -16,6 +16,7 @@ from customer_segment import EDA, Model, Evaluation
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 from tensorflow.keras.utils import plot_model
 import pickle
+import seaborn as sns
 
 # Constant
 DATA_PATH = os.path.join(os.getcwd(), "Data","train.csv")
@@ -26,6 +27,23 @@ MODEL_SAVE_PATH = os.path.join(os.getcwd(), 'Model','model.h5')
 #%%EDA
 
 df_raw = pd.read_csv(DATA_PATH)
+
+#%% Data Inspection using plotting
+# Gender against segmentation (A,B,C,D)
+sns.countplot(df_raw["Gender"], hue=df_raw["Segmentation"]) # Most male categorize under
+# D catergory and female and male do not have obvious differences in trend
+
+# profesion vs segmentation
+sns.countplot(df_raw["Profession"], hue=df_raw["Segmentation"]) # Only artist and
+# Healthcare show trend, others weak trend
+
+# ever_married vs segmentation
+sns.countplot(df_raw["Ever_Married"], hue=df_raw["Segmentation"]) # there's strong trend
+
+# [Altenative] How to compare groupby more than 2 cat and plot it?
+df_raw.groupby(["Segmentation", "Ever_Married", "Gender"]).agg({"Segmentation":"count"}).plot(kind="bar")
+
+#%% Data Cleaning
 X_raw = df_raw.iloc[:,1:10] # features only
 
 eda = EDA()
@@ -35,9 +53,11 @@ X_clean = eda.label_encoder(X_raw)
 # fill the NaN
 X_imputed = eda.simple_imputer(X_clean)
 
+#%% Feature Scalling
 # scale the features
 X_scaled = eda.feature_scaling(X_imputed)
 
+#%% Data Preprocessing
 # convert target to one_hot encoding
 y_raw = np.expand_dims(df_raw["Segmentation"], axis=-1)
 y_encoded = eda.one_hot_encoding(y_raw)
